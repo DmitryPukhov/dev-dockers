c587f31d5b63d872e51af31d651efa9347f8750e
dmitriy.pukhov <pukhov@gnivc.ru>
2025-11-03 17:32:51 +0300

    Greenplum dockerfile changed to rockylinux8

--- greenplum/build/install-dependencies.sh
+++ greenplum/build/install-dependencies.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+yum install -y python3-devel
+yum install -y python3-pip
+
 yum install -y epel-release
 yum install -y emacs-nox net-tools openssh-server openssh-clients passwd iproute procps vim go
 
 pip3 install psutil==5.7.0
+
